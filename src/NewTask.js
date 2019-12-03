04d9a9e511c7bb62e9eb42d72799472713f68a3b
Fiona C <fiona.c491@gmail.com>
2019-12-03 22:33:39 +0000

    Refactored Header and New Task Form to include bootstrap stylings in JSX format

--- src/NewTask.js
+++ src/NewTask.js
@@ -3,19 +3,25 @@ import React from "react"
 class NewTask extends React.Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-12 col-sm-12">
-                    <p>New Task</p>
-                </div>
-                <div className="col-12 col-sm-10">
-                    <p>Write Task Here</p>
-                    {/* Enter text and number here and is Urgent textbox */}
-                </div>
-                <div className="col-12 col-sm-2">
-                    <button className="btn btn-primary">Book</button>
-                </div>
+            <div className="container">
+            <form id="newTaskForm">
+                <div className="text-left">       
+                <div className="form-group">
+                <label for="addNewTask">New Task</label>
+                <input type="text" className="form-control" id="addNewTask" name="addNewTask"
+                    aria-describedby="addNewTaskHelp" placeholder="Write Task Here" maxlength="120" />
+                <small id="addNewTaskHelp" className="form-text text-muted">Enter text and numbers, max 120 characters.
+                </small>
             </div>
-        )
+                <div className="form-group form-check">
+                <input type="checkbox" className="form-check-input" id="taskUrgency" name="taskUrgency" />
+                <label className="form-check-label" for="taskUrgency">Is Urgent</label>
+            </div>
+            </div>  
+            <button type="button" className="btn btn-primary">Add</button>
+            </form>
+            </div>
+        );
     }
 }
 
