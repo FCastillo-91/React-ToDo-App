9c4a247c0bb310f4a2979739f6340c2b5be72cb8
Fiona C <fiona.c491@gmail.com>
2020-01-05 21:25:18 +0000

    Updates

---
+++ precache-manifest.1e7fbb1e4ac88a49dc19f4419c6b706c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebbf3a5233ff98e37d1a1a5ee35fbe2",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "a019562b6c15f62b0ebe",
+    "url": "/React-ToDo-App/static/css/main.1f2763e7.chunk.css"
+  },
+  {
+    "revision": "5ccef88f65cc160a4068",
+    "url": "/React-ToDo-App/static/js/2.39919258.chunk.js"
+  },
+  {
+    "revision": "a019562b6c15f62b0ebe",
+    "url": "/React-ToDo-App/static/js/main.17b1a00f.chunk.js"
+  },
+  {
+    "revision": "0939652f7d6ca27c1b4c",
+    "url": "/React-ToDo-App/static/js/runtime-main.4caef237.js"
+  }
+]);
