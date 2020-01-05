5d28ff73ddbe88f6ea917dd4d541685a5db63082
Fiona C <fiona.c491@gmail.com>
2020-01-05 19:29:27 +0000

    Edit button working

--- src/AddNewTaskForm.js
+++ src/AddNewTaskForm.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 class NewTask extends React.Component {
-    state = {
-        taskName: this.props.taskForm.name,
-        dueDate: this.props.taskForm.dueDate,
-        urgency: this.props.taskForm.urgency,
-    };
+    constructor(props) {
+        super(props);
+        console.log({
+            caca: props.taskForm
+        })
+        this.state = {
+            taskName: props.taskForm.name,
+            dueDate: props.taskForm.dueDate,
+            urgency: props.taskForm.urgency,
+        };
+    }
 
+    componentWillReceiveProps(props) {
+        this.setState({
+            taskName: props.taskForm.name,
+            dueDate: props.taskForm.dueDate,
+            urgency: props.taskForm.urgency
+        });
+    }
     updateTaskName = (event) => {
         this.setState({
             taskName: event.target.value
@@ -14,13 +27,13 @@ class NewTask extends React.Component {
     }
 
     updateDate = (event) => {
-        this.setState ({
+        this.setState({
             dueDate: event.target.value
-        }); 
+        });
     }
 
     updateUrgency = (event) => {
-        this.setState ({
+        this.setState({
             urgency: event.target.checked
         })
     }
@@ -39,19 +52,26 @@ class NewTask extends React.Component {
         return (
             <div className="row">
                 <div className="col-12 col-md-6 mb-2">
-                    <input type="text" onChange={this.updateTaskName} value={this.state.taskName} className="form-control" placeholder="Write Task Here"/>
+                    <input type="text" onChange={this.updateTaskName} value={this.state.taskName} className="form-control" placeholder="Write Task Here" />
                 </div>
                 <div className="col-8 col-md-3 mb-2">
-                    <input type="date" onChange={this.updateDate} value={this.state.dueDate} className="form-control"/>
+                    <input type="date" onChange={this.updateDate} value={this.state.dueDate} className="form-control" />
                 </div>
                 <div className="col-4 col-md-1 mb-2">
-                    <input type="checkbox" onChange={this.updateUrgency} className="form-check-input" id="taskUrgency" name="taskUrgency" />
+                    <input 
+                        type="checkbox" 
+                        onChange={this.updateUrgency} 
+                        className="form-check-input" 
+                        id="taskUrgency" 
+                        name="taskUrgency" 
+                        checked={this.state.urgency}
+                        />
                     <label className="form-check-label" hfor="taskUrgency">Is urgent</label>
                 </div>
                 <div className="col-12 col-md-2 mb-2">
                     <button onClick={this.addTask} className="btn btn-primary btn-block">Add</button>
                 </div>
-                
+
             </div>
         );
     }
--- src/App.js
+++ src/App.js
@@ -10,27 +10,23 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    taskList: [
-      { name: "Buy chicken", done: false, dueDate: "2019-12-02", urgency: true, id: uuid() },
-      { name: "Order Christmas presents", done: true, dueDate: "2019-11-30", urgency: false, id: uuid() },
-      { name: "Send flowers and New Home card to Anna&Joe", done: true, dueDate: "2019-12-01", urgency: true, id: uuid() },
-      { name: "Book hair appointment", done: false, dueDate: "2019-10-22", urgency: false, id: uuid() }
-    ],
-      taskForm : {
-        name: '',
-        dueDate: '2019-01-01',
-        urgency: true
-      }
+    taskList: [],
+    taskForm: {
+      name: '',
+      dueDate: '2019-01-01',
+      urgency: false
+    }
   }
 
   addNewTask = (name, date, urgency) => {
-
+    
     const newTask = {
       name: name,
       dueDate: date,
       done: false,
       urgency: urgency,
-      id: uuid()
+      id: uuid(),
+      isEditing: false
     }
 
     const copyOfTasks = this.state.taskList.slice()
@@ -52,7 +48,7 @@ class App extends React.Component {
       taskList: taskCompleted
     })
   }
-  
+
   deleteTask = (id) => {
     const myTaskList = this.state.taskList;
     const filteredTasks = myTaskList.filter(task => {
@@ -67,58 +63,58 @@ class App extends React.Component {
 
   editTask = (id) => {
     const myTaskList = this.state.taskList;
-    // const selectedTask = myTaskList.find(task => task.id === id);
+    
     let updateTask;
     myTaskList.forEach(task => {
-      if (task.id === id){
+      if (task.id === id) {
         updateTask = {
           name: task.name,
           dueDate: task.dueDate,
-          urgency: task.urgency,
+          urgency: task.urgency
         };
-      } 
+      }
     });
+
+    const selectedTask = myTaskList.filter(task => task.id !== id);
 
     this.setState({
-      taskForm: updateTask
+      taskForm: updateTask,
+      taskList: selectedTask
     });
-
   }
 
   render() {
     const completedTasks = this.state.taskList.filter(task => {
-        return (task.done)
+      return (task.done)
     });
 
     const pendingTasks = this.state.taskList.filter(task => {
-      return (task.done !== true) 
+      return (task.done !== true && task.isEditing === false)
     });
-
-    const taskForm = this.state.taskForm;
 
     return (
       <div className="App">
         <div className="container">
-        <Header />
-        <AddNewTaskForm 
-          addNewTaskFunc={this.addNewTask}
-          taskForm={this.state.taskForm}
+          <Header />
+          <AddNewTaskForm
+            addNewTaskFunc={this.addNewTask}
+            taskForm={this.state.taskForm}
           />
-        <ToDoListHeader count={pendingTasks.length} />
-        {pendingTasks.map((task) => {
-          return (
-            <Task 
-            key={task.id}
-            task={task}
-            taskCompletedFunc={this.completedTask}
-            taskDeletedFunc={this.deleteTask}
-            taskEditFunc={this.editTask}
-            />
-          );
-        })}
-        <DoneList 
-        completedTasks={completedTasks}
-        />
+          <ToDoListHeader count={pendingTasks.length} />
+          {pendingTasks.map((task) => {
+            return (
+              <Task
+                key={task.id}
+                task={task}
+                taskCompletedFunc={this.completedTask}
+                taskDeletedFunc={this.deleteTask}
+                taskEditFunc={this.editTask}
+              />
+            );
+          })}
+          <DoneList
+            completedTasks={completedTasks}
+          />
         </div>
       </div>
     );
