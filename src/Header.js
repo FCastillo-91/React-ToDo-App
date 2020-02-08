fa07ac2111adb75e8c43319b251d8a9aadac67e3
Fiona C <fiona.c491@gmail.com>
2020-02-08 15:21:02 +0000

    Resized the Things to Do Today header

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,12 @@ import './Header.css'
 class Header extends React.Component {
     render() {
         return (
-            <div className="jumbotron">
-                <h1 className="display-4">Things To Do Today</h1>
-                <p className="lead">"Organising is what you do before you do something, so that when you do it, it is not all mixed up."</p>
-                <p className="font-italic">A.A. Milne</p>
+            <div className="jumbotron py-4 mt-2">
+                <h1 className="display-5">Things To Do Today</h1>
+                <div className="d-none d-md-block">
+                    <p className="lead">"Organising is what you do before you do something, so that when you do it, it is not all mixed up."</p>
+                    <p className="font-italic">A.A. Milne</p>
+                </div>
             </div>
         );
     }
